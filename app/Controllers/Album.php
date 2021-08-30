8679be6c519ecbd91617b686259fe66151dcec4e
Werneck Oliveira <werneck.junior@infobase.com.br>
2021-08-30 17:35:21 -0300

    Tratamento para input vazio

--- app/Controllers/Album.php
+++ app/Controllers/Album.php
@@ -12,6 +12,26 @@ class Album extends BaseController
 	*/
 	public function search($value)
 	{
+		/* 
+			TODO: Werneck
+			O ideial fosse tratar o input com uma ferramento do framework
+			como middleware e entre outros. Devida a tempo a falta de tempo hábil
+			para desenvolver todas a funcionalidades, foi feita uma forma mais procedural
+			com o código abaixo:
+		*/
+		if((!isset($value) || trim($value)=='""' || trim($value)=='\'\'')){			
+			/*
+				O desejável fosse isoltar o este response em uma função separada,
+				E somente enviar os parâmetros(constantes) desejáveis, devido à
+				boas práticas de programação, repetir trecho de código.
+			*/ 
+			$this->response->setStatusCode(400);
+			return $this->response->setJSON([ 
+				'success' 	=> true, 
+				'code' 		=> HTTP_CODE_PARAM_ERROR, 
+				'result' 	=> 'O campo de busca não pode estar vazio'
+			]);
+		}
 
 		// Do search
 		$albummodel = new Albummodel();
